8d27c85f3a7ca992d6074138bceded79df4d673f
Jongho Kim <scurry2119@gmail.com>
2023-05-16 15:53:45 +0900

    FIX: You do not have to specify the value of optional parameter `blogSortMethod`

--- blog-search/src/main/kotlin/jongho/blog/blogsearch/data/BlogSortMethod.kt
+++ blog-search/src/main/kotlin/jongho/blog/blogsearch/data/BlogSortMethod.kt
@@ -1,10 +1,12 @@
 /**
  *   Definition of enum type used for request.
- *   If you use primitive type parameter, `@RequestParam` will automatically convert string value to your primitive type.
+ *   We use annotation `@RequestParam` (`org.springframework.web.bind.annotation.RequestParam`) to serve REST API.
+ *   If you use primitive type parameter, the `@RequestParam` will automatically convert string value to your primitive type.
  *   Else, you should implement converter using `org.springframework.core.convert.converter.Converter`.
  */
 package jongho.blog.blogsearch.data
 
+import jongho.blog.blogsearch.defaultBlogSortMethod
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.convert.converter.Converter
 
@@ -25,7 +27,7 @@ public class BlogSortMethodConverter : Converter<String, BlogSortMethod> {
         return when(source) {
             BlogSortMethod.ACCURACY.toString() -> BlogSortMethod.ACCURACY
             BlogSortMethod.RECENCY.toString()  -> BlogSortMethod.RECENCY
-            else -> null
+            else -> defaultBlogSortMethod   // NOTE: You do not have to specify the value of optional parameter `blogSortMethod
         }
     }
 }
